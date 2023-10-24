7f15ecd714dbeaeccba7315871f0210694dd04d2
RayCxggg <chexj18@163.com>
2023-10-24 16:22:39 +0800

    fix some compiler warnings

--- Mynewt/repos/apache-mynewt-nimble/firewall/libebpf/src/ebpf_env.c
+++ Mynewt/repos/apache-mynewt-nimble/firewall/libebpf/src/ebpf_env.c
@@ -6,24 +6,32 @@
 
 ebpf_env current_env;
 
-void init_ubpf_env() {
+void init_ubpf_env()
+{
     current_env.prog_list = NULL;
     current_env.tail = NULL;
     current_env.cur_size = 0;
 }
 
-ebpf_prog* ebpf_add_prog(const void *code, u32 code_len) {
-	ebpf_prog *prog = (ebpf_prog*) ebpf_malloc(sizeof(ebpf_prog));
+ebpf_prog *ebpf_add_prog(const void *code, u32 code_len)
+{
+    ebpf_prog *prog = (ebpf_prog *)ebpf_malloc(sizeof(ebpf_prog));
     prog->next = NULL;
     ebpf_vm *vm = ebpf_create();
     prog->vm = vm;
-    int ret = ebpf_vm_load(vm, code, code_len);
-    // prog
 
-    if (current_env.prog_list == NULL) {
+    int ret = ebpf_vm_load(vm, code, code_len);
+    if (ret != 0)
+    {
+        // printf("ebpf_vm_load failed\n");
+    }
+
+    if (current_env.prog_list == NULL)
+    {
         current_env.prog_list = prog;
     }
-    if (current_env.tail != NULL) {
+    if (current_env.tail != NULL)
+    {
         current_env.tail->next = prog;
     }
     current_env.tail = prog;
@@ -33,12 +41,13 @@ ebpf_prog* ebpf_add_prog(const void *code, u32 code_len) {
     return prog;
 }
 
-int ebpf_remove_prog(ebpf_prog *prog) {
+int ebpf_remove_prog(ebpf_prog *prog)
+{
     ebpf_vm_destroy(prog->vm);
     ebpf_free(prog);
     return RET_OK;
 }
 
-void destory_ebpf_env() {
-    
+void destory_ebpf_env()
+{
 }
--- Mynewt/repos/apache-mynewt-nimble/firewall/libebpf/src/jit.c
+++ Mynewt/repos/apache-mynewt-nimble/firewall/libebpf/src/jit.c
@@ -18,20 +18,20 @@ jit_mem g_jit_mem;
 
 jit_mem *jit_mem_allocate(int insts_num)
 {
-	jit_mem *mem = &g_jit_mem;
-	mem->code_size = sizeof(jit_buffer);
-	mem->jit_code = jit_buffer;
-	mem->jmp_offsets = offset_mem;
-	memset(jit_buffer, 0, sizeof(jit_buffer));
-	memset(offset_mem, 0, sizeof(offset_mem));
-	return mem;
+    jit_mem *mem = &g_jit_mem;
+    mem->code_size = sizeof(jit_buffer);
+    mem->jit_code = jit_buffer;
+    mem->jmp_offsets = offset_mem;
+    memset(jit_buffer, 0, sizeof(jit_buffer));
+    memset(offset_mem, 0, sizeof(offset_mem));
+    return mem;
 }
 
 void jit_mem_free(jit_mem *mem)
 {
-	mem->code_size = 0;
-	mem->jit_code = NULL;
-	mem->jmp_offsets = NULL;
+    mem->code_size = 0;
+    mem->jit_code = NULL;
+    mem->jmp_offsets = NULL;
 }
 
 #else
@@ -39,60 +39,61 @@ void jit_mem_free(jit_mem *mem)
 //
 jit_mem *jit_mem_allocate(int insts_num)
 {
-	jit_mem *mem = ebpf_malloc(sizeof(jit_mem));
-	mem->code_size = 10 * insts_num + 16;
-	mem->jit_code = ebpf_malloc(mem->code_size);
-	int offset_size = 4 * insts_num + 16;
-	mem->jmp_offsets = ebpf_malloc(offset_size);
+    jit_mem *mem = ebpf_malloc(sizeof(jit_mem));
+    mem->code_size = 10 * insts_num + 16;
+    mem->jit_code = ebpf_malloc(mem->code_size);
+    int offset_size = 4 * insts_num + 16;
+    mem->jmp_offsets = ebpf_malloc(offset_size);
 
-	// int total_size = sizeof(jit_mem) + mem->code_size + offset_size;
-	// printf("jit mem allocate size: %d\n", total_size);
+    // int total_size = sizeof(jit_mem) + mem->code_size + offset_size;
+    // printf("jit mem allocate size: %d\n", total_size);
 
-	memset(mem->jit_code, 0, mem->code_size);
-	memset(mem->jmp_offsets, 0, offset_size);
-	return mem;
+    memset(mem->jit_code, 0, mem->code_size);
+    memset(mem->jmp_offsets, 0, offset_size);
+    return mem;
 }
 
 void jit_mem_free(jit_mem *mem)
 {
-	ebpf_free(mem->jit_code);
-	ebpf_free(mem->jmp_offsets);
-	ebpf_free(mem);
+    ebpf_free(mem->jit_code);
+    ebpf_free(mem->jmp_offsets);
+    ebpf_free(mem);
 }
 
 #endif
 
 void jit_state_set_mem(jit_state *state, jit_mem *mem)
 {
-	state->jmem = mem;
-	state->jit_code = (uint8_t *)((uint32_t)mem->jit_code & (~0x3));
-	state->offsets = (uint32_t *)mem->jmp_offsets;
+    state->jmem = mem;
+    state->jit_code = (uint8_t *)((uint32_t)mem->jit_code & (~0x3));
+    state->offsets = (uint32_t *)mem->jmp_offsets;
 }
 
 void gen_jit_code(struct ebpf_vm *vm)
 {
-	if (vm->jmem != NULL) {
-		jit_mem_free(vm->jmem);
-	}
-	vm->jmem = jit_mem_allocate(vm->num_insts);
+    if (vm->jmem != NULL)
+    {
+        jit_mem_free(vm->jmem);
+    }
+    vm->jmem = jit_mem_allocate(vm->num_insts);
 
-	jit_state state;
-	state.insts = vm->insts;
-	state.inst_num = vm->num_insts;
-	state.idx = 0;
-	//state.jit_code = (uint8_t *) ((uint32_t) vm->jmem->jit_code & (~0x3));
-	state.err_line = 0;
-	state.__bpf_call_base = (uint32_t)vm->helper_func;
-	jit_state_set_mem(&state, vm->jmem);
-	jit_compile(&state);
-	vm->jit_func = (ebpf_jit_fn)((uint32_t)vm->jmem->jit_code | 0x1);
-	// jit_dump_inst(&state);
+    jit_state state;
+    state.insts = vm->insts;
+    state.inst_num = vm->num_insts;
+    state.idx = 0;
+    // state.jit_code = (uint8_t *) ((uint32_t) vm->jmem->jit_code & (~0x3));
+    state.err_line = 0;
+    state.__bpf_call_base = (void *)(uint32_t)vm->helper_func;
+    jit_state_set_mem(&state, vm->jmem);
+    jit_compile(&state);
+    vm->jit_func = (ebpf_jit_fn)((uint32_t)vm->jmem->jit_code | 0x1);
+    // jit_dump_inst(&state);
 #ifdef LINUX_TEST
-	// jit_dump_inst(&state);
+    // jit_dump_inst(&state);
 #endif
 
 #ifdef SYS_CORTEX_M4
-	__asm__("DSB");
-	__asm__("ISB");
+    __asm__("DSB");
+    __asm__("ISB");
 #endif
 }
